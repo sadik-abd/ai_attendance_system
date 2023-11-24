7d3c1069e5cb7cc976765fe95844e252f8467515
Sadique Abdullah <62884542+sadik-abd@users.noreply.github.com>
2023-11-24 17:00:30 +0600

    fixed previous bugs

--- AI/recogn.py
+++ AI/recogn.py
@@ -8,6 +8,8 @@ import pickle
 from insightface.model_zoo import model_zoo
 from yolo import YOLOv8_face
 import os
+import time 
+
 
 def crop_images(image, bounding_boxes):
     """
@@ -34,14 +36,11 @@ class Recogniser:
     def __init__(self, size) -> None:
         self.detector = YOLOv8_face("./models/yolov8n-face.onnx")
         self.tracker = None
-        self.rec_model = model_zoo.get_model(f'./models/w600k_mbf.onnx')
+        self.rec_model = model_zoo.get_model(f'./models/webface_r50.onnx', providers=["CUDAExecutionProvider"])
         self.rec_model.prepare(ctx_id=0, input_size=(640, 640), det_thres=0.5)
         self.embeds = {}
         for i in os.listdir("./asset/"):
             self.save_embeds("./asset/"+i,i.split(".")[0])
-        # self.recogniser = InceptionResnetV1(pretrained="vggface2",device="cpu").eval()
-        # weigths = torch.load("./models/face_net.pt")
-        #self.recogniser.load_state_dict(weigths)
         print("successfully loaded all the models")
 
     def save_embeds(self, path, name):
@@ -56,8 +55,7 @@ class Recogniser:
         with open('./embeds/embeds.pkl', 'wb') as file:
             pickle.dump(self.embeds, file)
 
-    def search_flatten(self, emb, threshold=0.5):
-        pred_names = []
+    def search_flatten(self, emb, threshold=0.99):
         known_names = [k for k in self.embeds.keys()]
         k_embs = [i for i in self.embeds.values()]
         scr = []
@@ -65,28 +63,27 @@ class Recogniser:
             scores = np.dot(emb, k_emb.T)
             score = np.clip(scores, 0., 1.)
 
-            idx = np.argmax(scores)
             scr.append(score)
         name = known_names[np.argmax(np.array(scr))]
-        # if np.argmax(np.array(scr)) < 0.2:
-        #     return "Unknown"
-        return name
+        if np.max(np.array(scr)) < threshold:
+            return "Unknown",0.0
+        return name, np.max(scr)
 
     def detect(self,img):
         results = self.detector.detect(img)
         return results
 
-    def draw(self, image, boxes, scores, kpts, name):
-        return self.detector.draw_detections(image, boxes, scores, kpts, name)
+    def draw(self, image, boxes, scores, kpts, name, scr):
+        return self.detector.draw_detections(image, boxes, scores, kpts, name, scr)
     
     def get_embeds(self,img, boxes, scores, kpts):
         n_kpts = []
-        kp = kpts[0]
+        kp = kpts
         for i in range(5):
             n_kpts.append([int(kp[i * 3]), int(kp[i * 3 + 1])])
-        face = Face(bbox=boxes[0], kps=np.array(n_kpts), det_score=scores[0])
-        return self.rec_model.get(img, face)
-
+        face = Face(bbox=boxes, kps=np.array(n_kpts), det_score=scores)
+        self.rec_model.get(img, face)
+        return face.normed_embedding
 
     def realtime_run(self):
         pass
@@ -95,7 +92,7 @@ if __name__ == "__main__":
     model = Recogniser("m")
 
     cap = cv2.VideoCapture(0)
-
+    prev_frame_time = time.time()
     if not cap.isOpened():
         print("Error: Unable to open camera")
         exit()
@@ -112,10 +109,28 @@ if __name__ == "__main__":
         
         if reslt is not None:
             boxes, scores, classids, kpts = reslt
-            name = model.search_flatten(model.get_embeds(frame, boxes, scores, kpts))
-            # Draw bounding boxes on the frame
-            frame = model.draw(frame, boxes, scores, kpts, name)
+            names, recg_score = [],[]
+            for boxe, score, kpt in zip(boxes, scores, kpts):
+                name,scor = model.search_flatten(model.get_embeds(frame, boxe, score, kpt ))#*[np.array(i,dtype=np.float64) for i in [boxe, score, kpt]]))
+                names.append(name)
+                recg_score.append(scor)
+                # Draw bounding boxes on the frame
+            frame = model.draw(frame, boxes, scores, kpts, names, recg_score)
             # Display the resulting frame
+            # Time when we finish processing for this frame
+        new_frame_time = time.time()
+
+        # Calculating the fps
+
+        # Convert the time to seconds
+        time_elapsed = new_frame_time - prev_frame_time
+        fps = 1/time_elapsed if time_elapsed > 0 else 0
+
+        # Updating the previous frame time to current time
+        prev_frame_time = new_frame_time
+
+        # Display the FPS on the frame
+        cv2.putText(frame, f'FPS: {int(fps)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 3, cv2.LINE_AA)
         cv2.imshow('Real-time Detection', frame)
 
         # Break the loop if 'q' is pressed
@@ -125,4 +140,3 @@ if __name__ == "__main__":
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
-        
