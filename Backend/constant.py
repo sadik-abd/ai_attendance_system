73d229e9628e6f775970bf83f1d106e3ab1272ca
Shaiful Islam <shaiful019@gmail.com>
2024-03-21 22:57:10 +0600

    Path Directory updated

--- Backend/constant.py
+++ Backend/constant.py
@@ -1,6 +1,16 @@
+# Sadik bhai
 
-RECORD_DB = "E:\dev\clients/attendance_system/code\Backend\database\data.json"
-IMAGES_PATH = "E:\dev\clients/attendance_system/code\Backend\database\images"
-DB_PATH = "E:\dev\clients/attendance_system/code\Backend\instance/attn_database.db"
-RECORD_IMAGE_PATH = "E:\dev\clients/attendance_system/code\Backend\database/record_img"
-GL_DB = "E:\dev\clients/attendance_system/code\Backend\database"
+# RECORD_DB = "E:\dev\clients/attendance_system/code\Backend\database\data.json"
+# IMAGES_PATH = "E:\dev\clients/attendance_system/code\Backend\database\images"
+# DB_PATH = "E:\dev\clients/attendance_system/code\Backend\instance/attn_database.db"
+# RECORD_IMAGE_PATH = "E:\dev\clients/attendance_system/code\Backend\database/record_img"
+# GL_DB = "E:\dev\clients/attendance_system/code\Backend\database"
+
+
+# Shaiful
+
+RECORD_DB = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\Backend\database\data.json"
+IMAGES_PATH = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\Backend\database\images"
+DB_PATH = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\Backend/instance/attn_database.db"
+RECORD_IMAGE_PATH = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\Backend\database/record_img"
+GL_DB = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\Backend/database"
