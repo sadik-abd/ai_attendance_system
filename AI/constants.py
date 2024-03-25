b644a34ff3ce60ad36d8f6d5d4fb2b8b97305460
sadik-abd <sadikabdullah20000000000000@gmail.com>
2024-03-26 02:23:14 +0600

    latest update

--- AI/constants.py
+++ AI/constants.py
@@ -1,7 +1,7 @@
-# EMBEDS_PATH = r"E:\dev\clients\attendance_system\code\AI\embeds"
-# IMAGES_PATH = r"E:\dev\clients/attendance_system\code\AI/asset"
-# MODELS_PATH = r"E:\dev\clients/attendance_system\code\AI\models"
+EMBEDS_PATH = r"E:\dev\clients\attendance_system\code\AI\embeds"
+IMAGES_PATH = r"E:\dev\clients/attendance_system\code\AI/asset"
+MODELS_PATH = r"E:\dev\clients/attendance_system\code\AI\models"
 
-EMBEDS_PATH = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\AI\embeds"
-IMAGES_PATH = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\AI\asset"
-MODELS_PATH = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\AI\models"
+# EMBEDS_PATH = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\AI\embeds"
+# IMAGES_PATH = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\AI\asset"
+# MODELS_PATH = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\AI\models"
