846c7528044ff2e667fca3a4cff8d99711c5b393
Sadique Abdullah <62884542+sadik-abd@users.noreply.github.com>
2024-04-06 03:32:31 +0600

    Update constants.py

--- AI/constants.py
+++ AI/constants.py
@@ -1,6 +1,6 @@
-EMBEDS_PATH = r"E:\dev\clients\attendance_system\code\AI\embeds"
-IMAGES_PATH = r"E:\dev\clients/attendance_system\code\AI/asset"
-MODELS_PATH = r"E:\dev\clients/attendance_system\code\AI\models"
+EMBEDS_PATH = r"./"
+IMAGES_PATH = r"./asset"
+MODELS_PATH = r"./models"
 
 # EMBEDS_PATH = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\AI\embeds"
 # IMAGES_PATH = r"C:\Users\Shaiful\Documents\GitHub\ai_attendance_system\ai_attendance_system\AI\asset"
